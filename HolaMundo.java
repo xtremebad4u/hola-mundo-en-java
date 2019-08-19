c503ab3486dd5a33c8ade354850a749025c62003
Julio de la Cruz <cesar.de.la.cruz.ovando@ericsson.com>
2019-08-19 15:36:15 -0600

    Comentarios añadidos al codigo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,10 @@
+/**
+*
+*Muestra por pantalla la frase "Hola mundo!"
+*
+* @author Julio Ovando
+*/
+
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");	
