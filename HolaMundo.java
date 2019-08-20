c06fa9398414ab9ceb85fb3193707e1e9567b827
Julio de la Cruz <cesar.de.la.cruz.ovando@ericsson.com>
2019-08-20 11:19:49 -0600

    Se corrige fin de linea

--- HolaMundo.java
+++ HolaMundo.java
@@ -8,7 +8,7 @@
 public class HolaMundo {
 	public static void main(String[] args) {
 		String naranja = "\033[33m";
-		String azul = "\033[34m"
+		String azul = "\033[34m";
 
 		System.out.println("Hola Mundo");
 		System.out.println("Aquí estamos haciendo pruebas con Git.");	
