58e0d1d7d26b58b8021b6739af3d23dc54173482
Julio de la Cruz <cesar.de.la.cruz.ovando@ericsson.com>
2019-08-20 10:47:38 -0600

    Saludo ampliado

--- HolaMundo.java
+++ HolaMundo.java
@@ -7,6 +7,7 @@
 
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");	
+		System.out.println("Hola Mundo");
+		System.out.println("Aquí estamos haciendo pruebas con Git.");	
 	}
 }
