9e161110ad8aefbee033ed84a9a11bdca519628c
Julio de la Cruz <cesar.de.la.cruz.ovando@ericsson.com>
2019-08-20 11:14:11 -0600

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -7,6 +7,9 @@
 
 public class HolaMundo {
 	public static void main(String[] args) {
+		String naranja = "\033[33m";
+		String azul = "\033[34m"
+
 		System.out.println("Hola Mundo");
 		System.out.println("Aquí estamos haciendo pruebas con Git.");	
 	}
